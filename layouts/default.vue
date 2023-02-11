1181c73735fb155239ab782bf635c5d8c28a46f1
juanwmedia <juan@wmedia.es>
2023-02-11 17:06:10 +0100

    feat: add static Home page

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full">
+    <div
+      class="w-full max-w-screen-lg px-8 pb-24 mx-auto mt-24 sm:mt-28 sm:px-16 slg:px-24 lg:mt-48"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
