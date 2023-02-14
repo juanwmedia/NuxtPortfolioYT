bbc9079247cfe30a3bdb9b3319500acf2935025e
juanwmedia <juan@wmedia.es>
2023-02-14 16:10:40 +0100

    feat: complete Hero component

---
+++ storyblok/AppButton.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+    default: () => ({}),
+  },
+});
+</script>
+
+<template>
+  <div v-editable="blok">
+    <a :href="blok.link.cached_url">{{ blok.text }}</a>
+  </div>
+</template>
---
+++ storyblok/AppIcon.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+    default: () => ({}),
+  },
+});
+</script>
+
+<template>
+  <div v-editable="blok">
+    <a
+      :href="blok.link.cached_url"
+      :title="blok.link.title"
+      :target="blok.link.target"
+    >
+      <img :src="blok.image.filename" :alt="blok.image.alt" />
+    </a>
+  </div>
+</template>
